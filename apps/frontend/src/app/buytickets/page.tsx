380df1b535e2acbb2f20af776fad5e8335adedc2
Jayanth Kumar <jknani111@gmail.com>
2023-12-29 23:41:10 +0530

    feat: Buy tickets page init
    - title component

---
+++ apps/frontend/src/app/buytickets/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "@repo/ui/navbar";
+import { ShowKindChooser } from "@repo/ui/showKindChooser";
+import { Title } from "@repo/ui/title";
+
+export default function BuyTickets() {
+  return (
+    <>
+      <Navbar />
+      <ShowKindChooser />
+      <Title />
+    </>
+  );
+}
---
+++ packages/ui/src/title.tsx
@@ -0,0 +1,23 @@
+export function Title() {
+  return (
+    <div className="title-div">
+      <div className="wrapper px-20 py-10">
+        <a href="" className="text-4xl">
+          Salaar
+        </a>
+        <div className="tags mt-3 flex flex-row">
+          {["UA","Action", "Adventure", "Fantasy", "Sci-Fi"].map((tag, index) => {
+            return (
+              <span
+                className="text-slate-500 text-xs uppercase px-2 mr-2 border border-solid border-stone-500 rounded-full"
+                key={index}
+              >
+                {tag}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
