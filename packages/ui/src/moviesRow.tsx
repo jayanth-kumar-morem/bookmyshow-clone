a0980a9abc2dc3fd3954766155b8072d228f2262
Jayanth Kumar <jknani111@gmail.com>
2024-01-02 00:58:46 +0530

    pre-commit-hook: Add linter

--- packages/ui/src/moviesRow.tsx
+++ packages/ui/src/moviesRow.tsx
@@ -1,43 +1,41 @@
-"use client";
-import { Carousel } from "./carousel";
+'use client'
+import { Carousel } from './carousel'
 
 export function MoviesRow() {
   let movies = [
     {
-      title: "Salaar: Cease Fire - Part 1",
+      title: 'Salaar: Cease Fire - Part 1',
       poster:
-        "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC40LzEwICA1NjAuM0sgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00301886-adtpyusrqu-portrait.jpg",
-      genre: ["Action", "Drama", "Thriller"],
+        'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC40LzEwICA1NjAuM0sgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00301886-adtpyusrqu-portrait.jpg',
+      genre: ['Action', 'Drama', 'Thriller'],
     },
-  ];
+  ]
   movies = [...movies, ...movies, ...movies, ...movies, ...movies, ...movies]
   movies = [...movies, ...movies, ...movies, ...movies, ...movies, ...movies]
   return (
     <div className="movies-row mt-14">
       <div className="wrapper flex flex-col px-40">
         <div className="title-see-all-btn-wrapper flex flex-row place-content-between mb-2">
-            <h2 className="leading-8 font-semibold text-xl">Recommended</h2>
-            <button className="text-red-500 text-sm">See All &gt;</button>
+          <h2 className="leading-8 font-semibold text-xl">Recommended</h2>
+          <button className="text-red-500 text-sm">See All &gt;</button>
         </div>
         <Carousel
           items={movies.map((movie, index) => (
             <MovieCard key={index} movie={movie} />
           ))}
-          settings ={
-            {
-                className: "center",
-                infinite: false,
-                centerPadding: "60px",
-                slidesToShow: 5,
-                slidesToScroll: 5,
-                swipeToSlide: true,
-                arrows: true,
-            }
-          }
+          settings={{
+            className: 'center',
+            infinite: false,
+            centerPadding: '60px',
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            swipeToSlide: true,
+            arrows: true,
+          }}
         />
       </div>
     </div>
-  );
+  )
 }
 
 function MovieCard({ movie }: { movie: any }) {
@@ -48,8 +46,8 @@ function MovieCard({ movie }: { movie: any }) {
       </div>
       <div className="movie-details">
         <h3 className="font-medium text-base pt-1">{movie.title}</h3>
-        <p className="text-slate-500 text-sm  pt-1">{movie.genre.join("/")}</p>
+        <p className="text-slate-500 text-sm  pt-1">{movie.genre.join('/')}</p>
       </div>
     </a>
-  );
+  )
 }
