78e5c02039ca4800e5b983abb876d51e45225b07
Jayanth Kumar <jknani111@gmail.com>
2024-01-07 00:14:05 +0530

    feat: refactor inits

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['@repo/ui'],
-}
+  transpilePackages: ["@repo/ui", "@repo/common", "@repo/state"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
